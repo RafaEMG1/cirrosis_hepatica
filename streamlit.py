4f426452634aa461f04e609eecb7badeac16e7c4
RafaEMG1 <rafaemg96@gmail.com>
2025-08-27 18:18:09 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -9,15 +9,6 @@ import os
 import altair as alt
 
 
-from sklearn.impute import SimpleImputer  # <- garantiza disponibilidad global
-
-# OneHotEncoder compatible con versiones (sparse_output vs sparse)
-from sklearn.preprocessing import OneHotEncoder
-try:
-    OH_ENCODER = OneHotEncoder(handle_unknown="ignore", sparse_output=False)
-except TypeError:
-    OH_ENCODER = OneHotEncoder(handle_unknown="ignore", sparse=False)
-
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
